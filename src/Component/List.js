156ee09d310a9491cd270a97532a793f5c8f697c
JeongHyeonSeok <hyeonseok45@gmail.com>
2022-12-04 20:01:00 +0900

    cors err 해결

--- src/Component/List.js
+++ src/Component/List.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const List = (props) => {
 
+    const [list, setList] = useState("")
+
     useEffect(() => {
+
+        // post 요청을 할때 클라이언트에서 데이터를 보내주기 위해 작성한 코드
+        let body = {
+            test: 'hello',
+        }
+        // .post("URL", 보낼 데이터) 형식으로 작성하여 클라이언트에서 데이터를 전송한다.
         axios
-            .post("/api/test")
+            .post("/api/test", body)
             .then((response) => {
                 // 성공 핸들링 / get이 성공했을때
-                alert("요청성공")
-                console.log(response)
+                setList(response.data.test)
             })
             .catch((error) => {
                 // 에러 핸들링 / get이 실패했을때
@@ -26,6 +33,7 @@ const List = (props) => {
                     <div key={index}>내용 : {content}</div>
                 )
             })}
+            <div>{list}</div>
         </div>
     )
 }
